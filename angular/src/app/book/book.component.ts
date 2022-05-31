42ac57c850579fff161349ec954865b64c2b9691
Pradip Shrestha <pradip.shrestha@amniltech.com>
2022-05-31 16:08:49 +0545

    Create done in UI successfully.

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -2,23 +2,65 @@ import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '@proxy/books';
 import { BookDto } from '@proxy/books/dto/models'
+import { bookTypeOptions } from '@proxy/books';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [ListService],
+  providers: [ListService,
+    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }
+  ],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+  
+  form: FormGroup;
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  // add bookTypes as a list of BookType enum members
+  bookTypes = bookTypeOptions;
+
+  isModalOpen = false;
+
+  constructor(public readonly list: ListService, private bookService: BookService, 
+    private fb: FormBuilder) {}
 
   ngOnInit() {
     const bookStreamCreator = (query) => this.bookService.getList(query);
 
     this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
       this.book = response;
+    });
+  }
+
+  // add new method
+  createBook() {
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      type: [null, Validators.required],
+      publishDate: [null, Validators.required],
+      price: [null, Validators.required],
+    });
+  }
+
+  // add save method
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.bookService.create(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 }
